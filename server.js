233981e4bb6b63e2b3439b4287e478dad983e784
Pavel <pavel.kunof@i-tribe.de>
2018-12-08 11:57:07 +0500

    [DEV] Server side additions

--- server.js
+++ server.js
@@ -9,34 +9,32 @@ var publicPath = path.resolve(__dirname, 'build');
 var getUserRoom = 0;
 var roomno = 1;
 
-http.on('request', function(request, response) {
-    var getRoom = request.url.split('room=')[1];
-    if (getRoom) {
-        getUserRoom = getRoom;
-    }
-});
-
 app.use(express.static(publicPath));
 app.get('/', function(req, res){
     res.sendFile('index.html', {root: publicPath});
 });
 
 io.on('connection', function (socket) {
-    var time = Date.now();
+    console.log(socket);
+    var getRoom = socket.handshake.headers['referer'].split('room=')[1];
+    if (getRoom) {
+        getUserRoom = getRoom;
+    }
 
     if (getUserRoom !== 0) {
-        roomno = getUserRoom
+        roomno = getUserRoom;
     } else {
-        roomno = time;
+        roomno = Date.now();
     }
+
+    //Если на 3000 порт подключается ещё игрок, то у первого меняется его комната
 
     //Increase roomno 2 clients are present in a room.
     if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
     socket.join("room-"+roomno);
 
     //Send this event to everyone in the room.
-    io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
-
+    io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. " + roomno);
 
     console.log(roomno);
 
@@ -47,7 +45,7 @@ io.on('connection', function (socket) {
             io.emit('goToPlacementShips', roomno);
         }
     });
-
+    
     io.emit('ready', roomno);
 });
 
--- src/js/speak.js
+++ src/js/speak.js
@@ -1,16 +1,25 @@
 var socket = io();
+var myRoom = 0;
+var roomLock = 0;
 
 $(function() {
     socket.on('connectToRoom',function(data) {
         console.log(data);
     });
 
-    socket.on('ready', function (user) {
-        $('#game-link').text(location.host + '?room=' + user)
+    socket.on('ready', function (room) {
+        if (roomLock === 0) {
+            roomLock = 1;
+            myRoom = room;
+            $('#game-link').text(location.host + '?room=' + room);
+            console.log(myRoom);
+        }
+
     }.bind(this));
 
-    socket.on('goToPlacementShips', function (user) {
-        alert(user);
-        $('#toPlacementShips').trigger('click');
+    socket.on('goToPlacementShips', function (room) {
+        if (room === myRoom) {
+            $('#toPlacementShips').trigger('click');
+        }
     }.bind(this));
 });
