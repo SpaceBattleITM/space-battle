a54d2969cea1c02c584f3d716d99330647794d50
Pavel <pavel.kunof@i-tribe.de>
2018-12-09 18:59:26 +0500

    [DEV] Совмещение событий с серверной и с клиентской части

--- src/js/speak.js
+++ src/js/speak.js
@@ -1,25 +1,31 @@
 var socket = io();
-var myRoom = 0;
-var roomLock = 0;
+var usersReady = 0;
 
 $(function() {
+    var $window = $(window);
+
     socket.on('connectToRoom',function(data) {
         console.log(data);
     });
 
     socket.on('ready', function (room) {
-        if (roomLock === 0) {
-            roomLock = 1;
-            myRoom = room;
-            $('#game-link').text(location.host + '?room=' + room);
-            console.log(myRoom);
-        }
-
+        $('#game-link').text(location.host + '?room=' + room);
     }.bind(this));
 
-    socket.on('goToPlacementShips', function (room) {
-        if (room === myRoom) {
-            $('#toPlacementShips').trigger('click');
+    //Данные что любой член комнаты расставил корабли
+    socket.on('playerReady', function () {
+        usersReady++;
+        $window.trigger('userReady');
+    }.bind(this));
+
+    //Сигнал что корабли данного клиента расставлены
+    $window.on('shipsReady', function() {
+        socket.emit('shipsReady');
+    });
+
+    socket.on('endGame', function () {
+        if (finalFlag === 0) {
+            endBattle(1);
         }
     }.bind(this));
 });
